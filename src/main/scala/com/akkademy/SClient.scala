19ac2e189b780dc5de14ffbcc4603ef696e17c54
Miguel Angel Moreno <miguel.s.address@gmail.com>
2016-03-07 22:55:08 +0100

    add functions to client to call server with SetIfNotExistsRequest, DeleteRequest and adding exampels in Main

--- src/main/scala/com/akkademy/SClient.scala
+++ src/main/scala/com/akkademy/SClient.scala
@@ -5,6 +5,7 @@ import akka.actor.ActorSystem
 import akka.pattern.ask
 import scala.concurrent.duration._
 import com.akkademy.messages._
+import scala.util.{Success, Failure}
 
 
 class SClient(remoteAddress: String){
@@ -16,12 +17,58 @@ class SClient(remoteAddress: String){
         remoteDb ? SetRequest(key, value)
     }
     
+    def setIfNotExists(key: String, value: Object) = {
+        remoteDb ? SetIfNotExistsRequest(key, value)
+    }
+
     def get(key: String) = {
         remoteDb ? GetRequest(key)
-    } 
+    }
+
+    def delete(key: String) = {
+        remoteDb ? DeleteRequest(key)
+    }
 }
 
 object Main extends App {
+    import scala.concurrent.ExecutionContext.Implicits.global
+
     val client = new SClient("127.0.0.1:2552")
+    println("Setting '123' = 123")
     client.set("123", new Integer(123))
+
+    println("Setting If Not Exists '123' = 456")
+    client.setIfNotExists("123", new Integer(456))
+
+    println("Setting If Not Exists '456' = 789")
+    client.setIfNotExists("456", new Integer(789))
+
+    client.get("123") onComplete {
+        case Success(value) => println("123 returned: " + value)
+        case Failure(ex) => println(ex)
+    }
+    client.get("456") onComplete {
+        case Success(value) => println("456 returned: " + value)
+        case Failure(ex) => println(ex)
+    }
+
+    client.get("789") onComplete {
+        case Success(value) => println("789 returned: " + value)
+        case Failure(ex) => println(ex.toString + ": couldnt get key 789, it does not exist")
+    }
+
+    client.delete("123") onComplete {
+        case Success(value) => println("deleted: key 123")
+        case Failure(ex) => println(ex.toString + ": couldnt delete key 123, it does not exist")
+    }
+
+    client.delete("456") onComplete {
+        case Success(value) => println("deleted: key 456")
+        case Failure(ex) => println(ex.toString + ": couldnt delete key 456, it does not exist")
+    }
+
+    client.delete("789") onComplete {
+        case Success(value) => println("deleted: key 789")
+        case Failure(ex) => println(ex.toString + ": couldnt delete key 789, it does not exist")
+    }
 }
--- src/test/scala/SClientIntegrationSpec.scala
+++ src/test/scala/SClientIntegrationSpec.scala
@@ -3,17 +3,57 @@ import org.scalatest.{FunSpecLike, Matchers}
 import scala.concurrent._
 import scala.concurrent.duration._
 import com.akkademy.SClient
+import com.akkademy.messages.KeyNotFoundException
 
 
 class SClientIntegrationSpec extends FunSpecLike with Matchers {
 
-    val client = new SClient("127.0.0.1:2552")
-    describe("akkademyDbClient") {
-        it("should set a value") {
-            client.set("123", new Integer(123))
-            val futureResult = client.get("123")
-            val result = Await.result(futureResult, 10 seconds)
-            result should equal(123)
+    var client = new SClient("127.0.0.1:2552")
+
+    describe("akkademyDb Scala Client") {
+        describe("set method") {
+            it("should set a value") {
+                client.set("123", new Integer(123))
+                val futureResult = client.get("123")
+                val result = Await.result(futureResult, 10 seconds)
+                result should equal(123)
+            }
+        }
+
+        describe("setIfNotExists method") {
+            it("should not set a value if it already exists") {
+                client.setIfNotExists("key", new Integer(123))
+                client.setIfNotExists("key", new Integer(555))
+                val futureResult = client.get("key")
+                val result = Await.result(futureResult, 10 seconds)
+                result should equal(123)
+            }
+
+            it("should set a value if not exists") {
+                //trick untill the server is mocked
+                client.setIfNotExists("newKey", new Integer(555))
+                val futureResult = client.get("newKey")
+                val result = Await.result(futureResult, 10 seconds)
+                result should equal(555)
+            }
+        }
+
+        describe("delete method") {
+            it("should delete a key if it exists") {
+                client.set("123", new Integer(123))
+                val futureResult = client.delete("123")
+                val result = Await.result(futureResult, 10 seconds)
+                result shouldBe a [java.lang.Boolean]
+                result should equal(true)
+            }
+
+            it("should fail with KeyNotFoundException when deleting a key that does not exist") {
+                val futureResult = client.delete("unknown")
+                val thrown = intercept[KeyNotFoundException] {
+                    Await.result(futureResult, 10 seconds)
+                }
+                thrown.key should equal("unknown")
+            }
         }
     }
 }
